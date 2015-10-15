6f541689f5f8eed21ed3306cb9b413573c336054
Burak Dede <burak@burakdede.com>
2015-10-15 23:54:15 +0300

    add first register implementation

--- src/main/java/Hoodie.java
+++ src/main/java/Hoodie.java
@@ -1,5 +1,28 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Proxy;
+
 /**
  * Created by burakdede on 15.10.15.
  */
 public class Hoodie {
+
+    private final static Logger logger = LoggerFactory.getLogger(Hoodie.class.getSimpleName());
+
+    public static <T> T registerNewTarget(Class<T> clazz, String baseUrl) {
+        // register new parser
+        T target;
+
+        ReflectiveMethodParser reflectiveMethodParser =
+                new ReflectiveMethodParser(clazz);
+
+        ReflectiveMethodParser.parse();
+
+        target = (T) Proxy.newProxyInstance(clazz.getClassLoader(),
+                                        new Class[]{clazz},
+                                        new ReflectiveInvocationHandler());
+
+        return target;
+    }
 }
