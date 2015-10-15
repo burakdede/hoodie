cd0f398d5a1240ca39bbebd5740d0c611b54472f
Burak Dede <burak@burakdede.com>
2015-10-16 00:13:34 +0300

    add logger for invocation debugging

--- src/main/java/ReflectiveInvocationHandler.java
+++ src/main/java/ReflectiveInvocationHandler.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -7,6 +10,8 @@ import java.util.Map;
  * Created by burakdede on 15.10.15.
  */
 public class ReflectiveInvocationHandler implements InvocationHandler {
+
+    private final static Logger logger = LoggerFactory.getLogger(ReflectiveInvocationHandler.class.getSimpleName());
 
     private static Map<Method, MethodMetadata> methodCache = new HashMap<>();
 
@@ -21,6 +26,7 @@ public class ReflectiveInvocationHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        logger.debug("Method invoked");
         return null;
     }
 }
