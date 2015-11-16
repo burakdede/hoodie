15746662c838031141da65f81ca040d05f1b84ff
Burak Dede <burak@burakdede.com>
2015-11-17 00:38:17 +0200

    make head request return type warning explicit

--- src/main/java/HoodieMetadataParser.java
+++ src/main/java/HoodieMetadataParser.java
@@ -86,7 +86,7 @@ public class HoodieMetadataParser {
                 logger.debug("Parsing return type");
                 Type t = m.getReturnType();
                 if (httpType.equalsIgnoreCase("HEAD") && m.getReturnType() != Response.class) {
-                    logger.error("HEAD only support Response as return type.");
+                    logger.error("HEAD only support javax.ws.rs.core.Response as return type.");
                 }
 
                 methodMetadata.setReturnType(t);
